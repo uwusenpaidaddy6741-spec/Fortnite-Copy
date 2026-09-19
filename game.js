64c7bab578584ffb7730621c730e5da4ec1ab2f0
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-19 19:11:21 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js";
 
 /*
@@ -2053,5 +2052,3 @@ updatePlayersUI();
 animate(
     performance.now()
 );
-```
-
